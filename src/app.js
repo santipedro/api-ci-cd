2e2c524dafeda20c37f0be286b46ea618d441fc3
Pedro Santiago <you@example.com>
2025-05-26 13:34:52 -0300

    Test Endpoint

--- src/app.js
+++ src/app.js
@@ -6,8 +6,8 @@ app.use(express.json());
 app.use('/api', routes);
 
 const startServer = () => {
-  return app.listen(process.env.PORT || 3000, () => {
-    console.log(`Servidor rodando na porta ${process.env.PORT || 3000}`);
+  return app.listen(process.env.PORT || 8080, () => {
+    console.log(`Servidor rodando na porta ${process.env.PORT || 8080}`);
   });
 };
 
